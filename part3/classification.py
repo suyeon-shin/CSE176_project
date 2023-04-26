ac1cc6332a3cecf0c65bb03168dc9a01dce21440
dgabriel17 <damiangabriel5@outlook.com>
2023-04-25 19:36:09 -0700

    pushed wrong file

--- part3/classification.py
+++ part3/classification.py
@@ -1,69 +1,35 @@
-# CSE 176 Project Part 3
-
-import numpy as np  # linear algebra
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
-import matplotlib.pyplot as plt  # data visualization
-import seaborn as sns  # statistical data visualization
-import json
-from sklearn.model_selection import train_test_split
+import pandas as pd
 import lightgbm as lgb
-from sklearn.feature_extraction import DictVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 
-# Load the json file into a var
-data_set = pd.read_json("News_Category_Dataset_v3.json", lines=True)
+# Load the text data into a pandas DataFrame
+data = pd.read_json("News_Category_Dataset_v3.json", lines=True)
 
-# Create vectorizor
-vec = CountVectorizer()
-# print(vec)
+# Preprocess the text data using CountVectorizer or TfidfVectorizer
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(data['headline'])
+y = data['category']
 
-# Tokenize and count work occurrences in the headlines
-# print(data_set['headline'])
-corpus = data_set['headline']
+# Split the preprocessed data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Fit the word occurences into the vectorizor and assign to a variable
-X = vec.fit_transform(corpus)
-
-# Tokenize all words that are at least two characters long
-analyze = vec.build_analyzer()
-for headline in data_set['headline']:
-    analyze(headline)
-
-# Retrieve words in their array indecies
-vec.get_feature_names_out()
-X.toarray()
-
-
-# Transformer for normalizing
-transformer = TfidfTransformer(smooth_idf=False)
-
-# Fit the headlines to the transformer
-megatron = transformer.fit_transform(X)
-print(megatron.toarray()) # Print them
-
-# Does everything that was just done but in one go
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# vectorizer = TfidfVectorizer()
-# vectorizer.fit_transform(corpus)
-
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(megatron, data_set['category'], test_size=0.2)
-
-# Create a LightGBM dataset object
-train_data = lgb.Dataset(X_train, label=y_train)
-
-# Set hyperparameters for the LightGBM model
+# Define a LightGBM model and its hyperparameters
 params = {
-    'objective': 'binary',
-    'metric': 'binary_logloss',
+    'objective': 'multiclass',
+    'metric': 'multi_logloss',
     'boosting_type': 'gbdt',
     'num_leaves': 31,
     'learning_rate': 0.05
 }
 
-# Train the LightGBM model
-model = lgb.train(params, train_data)
+model = lgb.LGBMClassifier(**params)
 
-# Evaluate the model on the testing set
+# Train the LightGBM model on the training data
+model.fit(X_train, y_train)
+
+# Evaluate the trained model on the testing data
 y_pred = model.predict(X_test)
+
+print(classification_report(y_test, y_pred))
