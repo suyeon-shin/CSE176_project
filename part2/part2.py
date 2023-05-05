4c89b58aad13f66e9e2fa54ab742ed79464c91b6
dgabriel17 <damiangabriel5@outlook.com>
2023-05-05 14:48:09 -0700

    Cleaning up the previous code

--- part2/part2.py
+++ part2/part2.py
@@ -1,16 +1,13 @@
-# from sklearn import datasets
-# import pdb
-
+# https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.LabelEncoder.html
+from sklearn.preprocessing import LabelEncoder
 from functions import *
-
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import RandomizedSearchCV
 from scipy.stats import uniform, randint
-
-import matplotlib.pyplot as plt
-
 import xgboost as xgb
 from xgboost import XGBClassifier
 import time
+import matplotlib.pyplot as plt
 
 # Start timer
 start_time = time.time()
@@ -26,37 +23,39 @@ x_train, y_train, x_test, y_test = load_data()
 
 # Need to transform the y_train value to fit xgboost.
 # y_train starts from 0 but not 1
-# https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.LabelEncoder.html
-from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 y_train = le.fit_transform(y_train)
 
-# Define the hyperparameter grid
+# Define the specific hyperparameters we're searching for
 param_dist = {'learning_rate': uniform(0, 1),
               'max_depth': randint(1, 10),
               'n_estimators': randint(50, 150),
               }
 
+# Declare the classifier
 model = XGBClassifier()
 
+# Declare the random search with our model
 random_search = RandomizedSearchCV(model, param_distributions=param_dist,
                                    n_iter=10, cv=5, scoring='accuracy', n_jobs=-1)
-
+# Fit the training data to the model
 model.fit(x_train, y_train)
 
-# Print the best hyperparameters
+# Print out the best hyperparameters
 print('Best hyperparameters:', random_search.get_params())
 
+# Test the model
 y_pred=model.predict(x_test)
 
 # End the timer and print out the time
 end_time = time.time()
 print(f"Execution time: {end_time - start_time} seconds")
 
+# Get and print out the accuracy
 accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy: %.2f%%" % ((1 - accuracy) * 100.0))
 
-from sklearn.metrics import confusion_matrix
+# Display the confusion matrix
 confusion = confusion_matrix(y_test, y_pred)
 print('Confusion Matrix\n')
 print(confusion)
