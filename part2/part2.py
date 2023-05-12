0f8400b4a1a487d013bfde68d3a46b60a9240cb6
suyeonnnnnn-shin <suzy7shin@gmail.com>
2023-05-11 22:50:04 -0700

    plots

--- part2/part2.py
+++ part2/part2.py
@@ -34,7 +34,33 @@ y_pred=model.predict(x_test)
 accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy: %.2f%%" % (accuracy * 100.0))
 
-from sklearn.metrics import confusion_matrix
-confusion = confusion_matrix(y_test, y_pred)
-print('Confusion Matrix\n')
-print(confusion)
+# from sklearn.metrics import confusion_matrix
+# confusion = confusion_matrix(y_test, y_pred)
+# print('Confusion Matrix\n')
+# print(confusion)
+
+import numpy as np
+
+# Define a list to store the test errors
+test_errors = []
+
+# Iterate over different values of the number of trees
+num_trees = [10, 50, 100, 150, 200]  # Update with your desired values
+for n in num_trees:
+    # Create the XGBoost classifier with the specified number of trees
+    model = XGBClassifier(n_estimators=n)
+    model.fit(x_train, y_train)
+    
+    # Predict on the test set
+    y_pred = model.predict(x_test)
+    
+    # Calculate the test error
+    test_error = 1 - accuracy_score(y_test, y_pred)
+    test_errors.append(test_error)
+
+# Plot the test errors as a function of the number of trees
+plt.plot(num_trees, test_errors, marker='o')
+plt.xlabel('Number of Trees')
+plt.ylabel('Test Error')
+plt.title('Test Error vs. Number of Trees')
+plt.show()
